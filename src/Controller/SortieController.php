858352d7379d80aa5fc59469a544b63e0444ca0f
BertrandC44 <courgeau.bertrand@icloud.com>
2025-08-26 14:57:49 +0200

    Add template :  - index.html.twig Add Controller :  - SortieController.php

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/sortie', name: 'sortie')]
 final class SortieController extends AbstractController
 {
-    #[Route('/sortie', name: 'app_sortie')]
-    public function index(): Response
+    #[Route('/', name: '')]
+    #[IsGranted('ROLE_USER')]
+    public function index(SortieRepository $sortieRepository): Response
     {
+        $sorties = $sortieRepository->findAll();
         return $this->render('sortie/index.html.twig', [
-            'controller_name' => 'SortieController',
+            'sorties' => $sorties,
         ]);
+    }
+
+
+    #[Route('/edit',name: '_edit')]
+public function editSortie(Request $request, EntityManagerInterface $em) : Response
+    {
+        $sortie = new Sortie();
+        $form = $this->createForm(SortieType::class, $sortie);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $em->persist($sortie);
+            $em->flush();
+
+            $this->addFlash('success', 'Nouvelle sortie créée');
+            return $this->redirectToRoute('sortie');
+        }
+
+        return $this->render('sortie/edit.html.twig',[
+            'sortie_form' => $form,]);
+
     }
 }
