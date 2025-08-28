994738a9c62266cf66f6274c3953cdcb1332cd92
NathBillaud <talie.nanou@gmail.com>
2025-08-28 16:52:07 +0200

    upload photo à finir

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -72,6 +72,20 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\JoinColumn(nullable: false)]
     private ?Site $site = null;
 
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $photo = null;
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): self
+    {
+        $this->photo = $photo;
+        return $this;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
