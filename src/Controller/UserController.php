94a4f73ddcb8df6d08b45754a1ec8211347bf0be
NathBillaud <talie.nanou@gmail.com>
2025-08-29 12:54:06 +0200

    upload photo +condition d'accès juste à lutilisateur sur template profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,7 @@ use App\Entity\Sortie;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -26,6 +27,26 @@ final class UserController extends AbstractController
         return $this->render('user/profil.html.twig', [
             'user' => $user,
         ]);
+    }
+
+
+    #[Route('/{id}/upload-photo', name: '_upload_photo')]
+    public function uploadPhoto(Request $request, User $user, EntityManagerInterface $em): Response
+    {
+        if ($this->getUser()->getId() !== $user->getId()) {
+            throw $this->createAccessDeniedException('Accès refusé.');
+        }
+        $file = $request->files->get('photo');
+        if ($file) {
+            $filename = uniqid() . '.' . $file->guessExtension();
+            $file->move($this->getParameter('photo_directory'), $filename);
+            $user->setPhoto($filename);
+            $em->flush();
+
+            $this->addFlash('success', 'Photo mise à jour.');
+        }
+
+        return $this->redirectToRoute('user_profil', ['id' => $user->getId()]);
     }
 
 
