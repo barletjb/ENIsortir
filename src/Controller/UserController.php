cb3dff63ebae64f799b5b658bb109f595601b4fd
BertrandC44 <courgeau.bertrand@icloud.com>
2025-09-01 10:42:32 +0200

    Affichage synchro + update profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
-use App\Helper\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
