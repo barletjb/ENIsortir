4f8e94705c2e9f4505f72f17639392e6a321c618
hln44 <helenepog@aol.fr>
2025-09-03 12:21:48 +0200

    update / css feat / ux mobile

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,12 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+
 import './vendor/bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
+
+import './styles/navbar.css';
+import './styles/boutons.css';
+import './styles/usersList.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
